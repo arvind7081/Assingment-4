6b6f963691596943fddac3e9f20360094fbc7c70
arvind7081 <79131871+arvind7081@users.noreply.github.com>
2022-09-14 20:42:10 +0530

    Add files via upload

---
+++ nine.c
@@ -0,0 +1,12 @@
+//9. Write a program to print cube of the first 10 natural number.
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d ",i*i*i);
+        i++;
+    }
+    return 0;
+}
---
+++ two.c
@@ -0,0 +1,11 @@
+//2. Write a program to print the first 10 natural numbers.
+int main()
+{
+    int i=1;
+    while (i<=10)
+    {
+        printf("%d ",i);
+        i++;
+    }
+    return 0;
+}
